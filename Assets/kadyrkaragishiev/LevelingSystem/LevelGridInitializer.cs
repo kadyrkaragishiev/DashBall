8b306ac2e5e467d4d71688ebb139b2be3bb0e64c
Abdulkadir Karagishiev <abdulkadyr.karagishiev@ninsar.pro>
2022-07-26 12:39:57 +0300

    feat: add levels and remake level system

--- Assets/kadyrkaragishiev/LevelingSystem/LevelGridInitializer.cs
+++ Assets/kadyrkaragishiev/LevelingSystem/LevelGridInitializer.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using kadyrkaragishiev.Scripts;
+using kadyrkaragishiev.UI;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
--- Assets/kadyrkaragishiev/LevelingSystem/LevelSettings.cs
+++ Assets/kadyrkaragishiev/LevelingSystem/LevelSettings.cs
@@ -10,5 +10,14 @@ namespace kadyrkaragishiev.LevelingSystem
         public float WrongPlatformsChance;
         public float PlatformsSpeed;
         public bool isUnlocked;
+        
+        [ContextMenu("SetRandomLevel")]
+        public void SetRandomLevel()
+        {
+            DistanceBetweenPlatforms = Mathf.Round(Random.Range(0.9f, 1.5f)*100)/100;
+            LevelLength = Random.Range(40, 60);
+            WrongPlatformsChance = Mathf.Round(Random.Range(0.4f, 0.7f));
+            PlatformsSpeed = Mathf.Round(Random.Range(30f, 40f));
+        }
     }
 }
